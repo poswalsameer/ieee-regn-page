b81fc4829ccecd8a216d8eef23533b352d04ed9a
Ayush Ranjan <ranaayush0725@gmail.com>
2024-02-04 14:04:55 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import { ZodError, z } from "zod";
-
 import sheets, { SHEET_ID } from "./sheetclient.js";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 
 const contactFormSchema = z.object({
   name: z.string().min(1, { message: "Name is required" }),
+  rollNumber: z.string().min(1, { message: "Roll Number is required" }),
+  year: z.string().min(1, { message: "Year is required" }),
+  branch: z.string().min(1, { message: "Branch is required" }),
   email: z.string().email(),
-  message: z.string().min(1, { message: "Message is required" }),
+  phoneNumber: z.string().min(1, { message: "Phone Number is required" }),
+  skills: z.string().min(1, { message: "Skills is required" }),
 });
 
 app.use(express.json());
@@ -23,6 +30,7 @@ app.post("/send-message", async (req, res) => {
     const rows = Object.values(body);
 
     console.log(rows);
+    console.log(import.meta);
 
     await sheets.spreadsheets.values.append({
       spreadsheetId: SHEET_ID,
@@ -33,7 +41,8 @@ app.post("/send-message", async (req, res) => {
         values: [rows],
       },
     });
-    res.json({ message: "Data added successfully" });
+    //res.json({ message: "Data added successfully" });
+    res.redirect("/success");
   } catch (error) {
     if (error instanceof ZodError) {
       res.status(400).json({ error: error.message });
@@ -41,6 +50,10 @@ app.post("/send-message", async (req, res) => {
       res.status(400).json({ error: error });
     }
   }
+});
+
+app.get("/success", (req, res) => {
+  res.sendFile(`${__dirname}/success.html`);
 });
 
 app.listen(5000, () => console.log("App running on http://localhost:5000"));
