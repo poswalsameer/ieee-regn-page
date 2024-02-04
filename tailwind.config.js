6e523b6a613fb96e08a8bde0a5d5a5d7ad64325c
Ayush Ranjan <ranaayush0725@gmail.com>
2024-02-04 21:43:47 +0530

    backend  commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bungeeinline: ['"Bungee Inline", sans-serif'],
+        orbitron: ['"Orbitron", sans-serif'],
+        playfair: ['"Playfair Display", serif'],
+        roboto: ['"Roboto", sans-serif'],
+        poppins: ['"Poppins", sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
